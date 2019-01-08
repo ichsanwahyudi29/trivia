500b7096e23b860471424d85bd7461715d2e3d37
Ichsan Wahyudi <ichsan.sano@gmail.com>
2019-01-08 09:22:20 +0700

    config

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+console.log('modee', process.env.NODE_ENV);
+
+const webpackConfig = {
+  mode: process.env.NODE_ENV,
+  devServer: {
+    contentBase: './dist',
+    port: 3333,
+  },
+  devtool: 'source-map',
+  entry: {
+    main: ['./src/js/index.js', './src/scss/style.scss'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProduction ? '[name].[contenthash].min.js' : '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/img',
+              limit: 8192,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].ext',
+              outputPath: 'assets/font',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? '[name].[contenthash].min.css' : '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
+};
+
+module.exports = webpackConfig;
